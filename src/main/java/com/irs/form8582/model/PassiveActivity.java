d9c3b3c1de41e92541ac059607df63bbdaa3b452
Michael James <41578568+nycpivot@users.noreply.github.com>
2025-10-05 20:27:14 -0400

    chore(codegen): SCRUM-23

---
+++ src/main/java/com/irs/form8582/model/PassiveActivity.java
@@ -0,0 +1,48 @@
+package com.irs.form8582.model;
+
+import javax.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "passive_activities")
+public class PassiveActivity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "activity_name", nullable = false)
+    private String activityName;
+
+    @Column(name = "activity_type")
+    private String activityType; // RENTAL_REDL_ESTATE, TRADE_BUSINESS, etc.
+
+    @Column(name = "current_year_income")
+    private BigDecimal currentYearIncome;
+
+    @Column(name = "current_year_loss")
+    private BigDecimal currentYearLoss;
+
+    @Column(name = "prior_year_unallowed_loss")
+    private BigDecimal priorYearUnallowedLoss;
+
+    @Column(name = "active_participation")
+    private Boolean activeParticipation;
+
+    @Column(name = "material_participation")
+    private Boolean materialParticipation;
+
+    @Column(name = "tax_year")
+    private Integer taxYear;
+
+    @Column(name = "created_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+}
