3dc75590af2073ce3d36b39734a191989d52f7ed
Michael James <41578568+nycpivot@users.noreply.github.com>
2025-10-05 20:27:08 -0400

    chore(codegen): SCRUM-23

---
+++ src/main/java/com/irs/form8582/Form8582Application.java
@@ -0,0 +1,12 @@
+package com.irs.form8582;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Form8582Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Form8582Application.class, args);
+    }
+}
