e09dd5088a523427888e50cb3ce00546db317b89
Michael James <41578568+nycpivot@users.noreply.github.com>
2025-10-08 08:51:03 -0400

    chore(codegen): SCRUM-23

--- src/main/java/com/irs/form8582/repository/PassiveActivityRepository.java
+++ src/main/java/com/irs/form8582/repository/PassiveActivityRepository.java
@@ -2,8 +2,6 @@ package com.irs.form8582.repository;
 
 import com.irs.form8582.model.PassiveActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -14,6 +12,5 @@ public interface PassiveActivityRepository extends JpaRepository<PassiveActivity
 
     List<PassiveActivity> findByActivityType(String activityType);
 
-    @Query("SELECT p FROM PassiveActivity p WHERE p.taxYear = :taxYear AND p.activeParticipation = true")
-    List<PassiveActivity> findActiveParticipationActivities(@Param("taxYear") Integer taxYear);
+    List<PassiveActivity> findByActiveParticipationTrue();
 }
